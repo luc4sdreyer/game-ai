05e188d25e5c9f82f6a712841f57f11fe2b3f504
Lucas Dreyer <luc4sdreyer@gmail.com>
2013-02-13 23:31:55 +0200

    Added timing measurements to test2

--- example/TestGameState.js
+++ example/TestGameState.js
@@ -152,64 +152,115 @@ function testBots1() {
 }
 
 // Play Minimax bot vs Random bot, and test if AlphaBeta pruning vs no pruning always produces the same move
+// Also compares the time needed to search to a given depth with different options
 function testBots2() {
-    var gameAI = require('../src/GameAI')
-    var b = require('./bots/Bots');
-    var state = new g.GameState();
-    var minibots = [
-        [true , null, true, 4],
-        [false, null, true, 4]
-        ];
+    var depthSearched  = [2,    3,      4,  5,  6];
+    var numGamesToPlay = [1500, 200,    20, 4,  1];
+    var averages = [];
+    var averagesNormalized = [];
 
-    var numGames = 50;
-    results = new Array(2);
-    for (var bot1 = 0; bot1 < 2; bot1++) {
-        results[bot1] = 0;
-    }
+    for (var d = 0; d < depthSearched.length; d++) {
+        averages.push([]);
+        averagesNormalized.push([]);
+        var gameAI = require('../src/GameAI')
+        var b = require('./bots/Bots');
+        var state = new g.GameState();
+        var minibots = [
+            [false , null, true, depthSearched[d]],
+            [true, null, true, depthSearched[d]]
+            ];
 
-    for (var gameNr = 0; gameNr < numGames; gameNr++) {
-        if (Math.floor(gameNr/(numGames)*100) % 10 === 0) {
-            console.log(Math.floor(gameNr/(numGames)*100) +"% complete");
+        var timePerMove = [];
+        for (var k = 0; k < minibots.length; k++) {
+            timePerMove.push([]);
         }
-        state = new g.GameState();
-        var playerNum = 0;
-        while (state.isGameOver() !== true) {
-            var bestPaths = new Array(minibots.length);
-            for (var m = 0; m < minibots.length; m++) {
-                var search = new gameAI.Search();
-                bestPaths[m] = search.MinimaxID(state, minibots[m][0], minibots[m][1], minibots[m][2], minibots[m][3]);
+
+        var numGames = numGamesToPlay[d];
+        results = new Array(2);
+        for (var bot1 = 0; bot1 < 2; bot1++) {
+            results[bot1] = 0;
+        }
+
+        for (var gameNr = 0; gameNr < numGames; gameNr++) {
+            //if (Math.floor(gameNr/(numGames)*100) % 10 === 0) {
+            if (gameNr % Math.floor(numGames/10) === 0) {
+                console.log(Math.floor(gameNr/(numGames)*100) +"% complete");
             }
-            for (var i = 0; i < bestPaths[0].length; i++) {
-                for (var j = 1; j < bestPaths.length; j++) {
-                    if (bestPaths[j][i] !== bestPaths[j-1][i]) {
-                        console.log("Discrepancy found!");
+            state = new g.GameState();
+            var playerNum = 0;
+            var move = -1;
+            var success = false;
+            while (state.isGameOver() !== true) {
+                if (gameNr*2 >= numGames) {
+                    var rBot = new b.Random();
+                    move = rBot.getMove(state);
+                    success = state.move(move);
+                    if (success !== true) {
+                        console.log(" provided invalid move ("+move+"), game ended");
                         break;
                     }
-                }                
-            }
-            //console.log(bestPaths[0] +" === "+ bestPaths[1]);
-            var move = bestPaths[0][0];
-            var success = state.move(move);
-            if (success !== true) {
-                console.log(" provided invalid move ("+move+"), game ended");
-                break;
-            } 
-            if (state.isGameOver() === true) {
-                results[0]++;
-            }
+                    if (state.isGameOver() === true) {
+                        break;
+                    }
+                }
+                var bestPaths = new Array(minibots.length);
+                for (var m = 0; m < minibots.length; m++) {
+                    var search = new gameAI.Search();
+                    var startTime = process.hrtime();
+                    bestPaths[m] = search.MinimaxID(state, minibots[m][0], minibots[m][1], minibots[m][2], minibots[m][3]);
+                    var diff = process.hrtime(startTime);
+                    timePerMove[m].push((diff[0] * 1e9 + diff[1]));
+                }
+                for (var i = 0; i < bestPaths[0].length; i++) {
+                    for (var j = 1; j < bestPaths.length; j++) {
+                        if (bestPaths[j][i] !== bestPaths[j-1][i]) {
+                            console.log("Discrepancy found!");
+                            break;
+                        }
+                    }                
+                }
+                //console.log(bestPaths[0] +" === "+ bestPaths[1]);
+                move = bestPaths[0][0];
+                success = state.move(move);
+                if (success !== true) {
+                    console.log(" provided invalid move ("+move+"), game ended");
+                    break;
+                } 
+                if (state.isGameOver() === true) {
+                    results[0]++;
+                }
 
-            var rBot = new b.Random();
-            move = rBot.getMove(state);
-            success = state.move(move);
-            if (success !== true) {
-                console.log(" provided invalid move ("+move+"), game ended");
-                break;
-            }
-            if (state.isGameOver() === true) {
-                break;
+                if (gameNr*2 < numGames) {
+                    var rBot = new b.Random();
+                    move = rBot.getMove(state);
+                    success = state.move(move);
+                    if (success !== true) {
+                        console.log(" provided invalid move ("+move+"), game ended");
+                        break;
+                    }
+                    if (state.isGameOver() === true) {
+                        break;
+                    }
+                }
             }
         }
+
+        var sums = new Array(minibots.length);
+        for (var i = 0; i < minibots.length; i++) {
+            sums[i] = 0;
+            for (var j = 0; j < timePerMove[0].length; j++) {
+                sums[i] += timePerMove[i][j];
+            }
+            averages[d].push(Math.round(sums[i]/timePerMove[0].length/1000));
+        }
+        for (var i = 0; i < minibots.length; i++) {
+            averagesNormalized[d].push(Math.round(sums[0]/sums[i]*100));
+        }
+        console.log("final score for depth "+d+": "+String(results));
     }
-    console.log("results: "+String(results));
+    for (var d = 0; d < depthSearched.length; d++) {
+        console.log("Depth = "+depthSearched[d]+": abs time avg (us): "+String(averages[d]));
+        console.log("Depth = "+depthSearched[d]+": time avg increase: "+String(averagesNormalized[d]));
+    }
     
 }
